0b05daa6dee50bace36d8608110b06e6d9009122
Ladislav Kotrba <lkotrba@volny.cz>
2020-01-10 14:22:13 +0100

    trocho podrobnější varianta

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main (int argc, char ** argv)
 
 	printf ("Zadejte své jméno:"); 
 	fgets (jméno, 255, stdin); 
+	printf ("length =% d \ n", strlen (name)); / * ladicí øádek * / 
+	jméno [strlen (name) -1] = '\ 0'; / * odeberte nový øádek na konci * / 
 
 	printf ("Hello% s! \ n", jméno); 
 	návrat 0; 
