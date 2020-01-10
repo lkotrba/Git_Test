31330a0dd640a54e8a3bffd3408a4a98db810a1f
Ladislav Kotrba <lkotrba@volny.cz>
2020-01-10 14:21:33 +0100

    Trochu podrobnější varianta

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> 
+#include <string.h> 
 
-int main(int argc, char **argv)
-{
-	printf("Hello world!\n");
-	return 0;
+int main (int argc, char ** argv) 
+{ 
+	char name [255]; 
+
+	printf ("Zadejte své jméno:"); 
+	fgets (jméno, 255, stdin); 
+
+	printf ("Hello% s! \ n", jméno); 
+	návrat 0; 
 }
