f874a9ef0fb9eaa7547d3e5a37bded5743b8cbec
Ladislav Kotrba <lkotrba@volny.cz>
2020-01-11 02:01:10 +0100

    Oprava gramatické chyby

--- main.c
+++ main.c
@@ -10,6 +10,6 @@ int main (int argc, char ** argv)
 	printf ("length =% d \ n", strlen (name)); / * ladicí øádek * / 
 	jméno [strlen (name) -1] = '\ 0'; / * odeberte nový øádek na konci * / 
 
-	printf ("Hello% s! \ n", jméno); 
+	printf ("Hello,% s! \ n", jméno); 
 	návrat 0; 
 }
