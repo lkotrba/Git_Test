12b1f77973b56d871b26b95923b970cf3303a54e
Ladislav Kotrba <lkotrba@volny.cz>
2020-01-11 02:40:30 +0100

    Přesun dotazování na jméno do vlastního souboru a přechod do angličtiny.

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
-#include <stdio.h> 
-#include <string.h> 
+/* main.c */
+#include <stdio.h>
 
-int main (int argc, char ** argv) 
-{ 
-	char first [255], last [255]; 
+#include "askname.h"
 
-	printf ("Zadejte své jméno:"); 
-	fgets (first, 255, stdin); 
-	first [strlen (first) -1] = '\ 0'; / * odstranit nový øádek na konci * / 
-    
-    printf ("Nyní zadejte své pøíjmení:"); 
-	get (last); /* pøeteèení zásobníku? co je to? * / 
+int main(int argc, char **argv)
+{
+	char first[255], last[255];
 
-	printf ("Ahoj% s% s! \ n", first, last); 
- 	retunn 0; 
->>>>>>> c21b5fc1bca19ccb37ea83f0e1f4a1fdd8312e2e
+	askname(first, last);
+
+	printf("Hello, %s %s!\n", first, last);
+ 	return 0;
 }
